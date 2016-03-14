b0785b44835d563435a2ba375929b6e2fcdce7cb
Jill Kuchman <pillanoid@gmail.com>
2016-03-14 15:02:59 -0700

    Initial commit. Working app until end of video.

---
+++ src/AJAXTest01/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using AJAXTest01.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AJAXTest01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HelloAjax()
+        {
+            return Content("Hello Ajax", "text/plain");
+        }
+
+        public IActionResult Sum(int a, int b)
+        {
+            return Content((a+ b).ToString(), "text/plain");
+        }
+
+        public IActionResult DisplayObject()
+        {
+            Product p1 = new Product("pr1", "Name 1", 1000);
+            return Json(p1);
+        }
+
+        public IActionResult DisplayListObject()
+        {
+            List<Product> listProduct = new List<Product>();
+            listProduct.Add(new Product("pr1", "Name 1", 1000));
+            listProduct.Add(new Product("pr2", "Name 2", 2000));
+            listProduct.Add(new Product("pr3", "Name 3", 3000));
+            return Json(listProduct);
+        }
+    }
+}
---
+++ src/AJAXTest01/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAXTest01.Models
+{
+    public class Product
+    {
+        private string _id;
+        private string _name;
+        private decimal _price;
+
+        public Product() { }
+
+        public Product(string id, string name, decimal price)
+        {
+            _id = id;
+            _name = name;
+            _price = price;
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+
+    }
+}
